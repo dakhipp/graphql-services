138eca8dc12cf01ada217600918e85cbe32a70f7
Dakota Hipp <dakhipp@gmail.com>
2018-09-01 01:05:06 -0700

    tests finally pass and fail as they should

--- tests/integration/register_test.go
+++ tests/integration/register_test.go
@@ -1,17 +1,30 @@
 package register
 
 import (
+	"log"
 	"testing"
 
 	. "github.com/franela/goblin"
+	"github.com/kelseyhightower/envconfig"
 
 	"github.com/vektah/gqlgen/client"
 )
 
+// Config : Configuration values created from environment variables
+type Config struct {
+	GQLEndpoint string `envconfig:"GQL_ENDPOINT"`
+}
+
 func TestRegister(t *testing.T) {
 	g := Goblin(t)
+	// Declare and attempt to cast config struct
+	var cfg Config
+	err := envconfig.Process("", &cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	c := client.New("http://0.0.0.1:8000/graphql")
+	c := client.New(cfg.GQLEndpoint)
 
 	g.Describe("Register", func() {
 		g.It("Should create a new user ", func() {
